6d69c609dc2732e5ad459dd4c37ffb7197a39870
Niels Martignène <niels.martignene@gmail.com>
2015-10-23 15:18:45 +0200

    Add log functions and levels

--- group__misc.js
+++ group__misc.js
@@ -8,13 +8,18 @@ var group__misc =
     [ "HS_VERSION", "group__misc.html#ga28f92c4d1e1785c7fc653ee1cec7c35d", null ],
     [ "HS_VERSION_STRING", "group__misc.html#ga523c656c82be06903d607e2dbb88e3d1", null ],
     [ "hs_descriptor", "group__misc.html#ga9fa7e0b22e44f003eac46a8b1d911793", null ],
-    [ "hs_err", "group__misc.html#gaf987ed07669903194c9f28386fc0c29b", [
-      [ "HS_ERROR_MEMORY", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29ba0a0410182743d83784cea0cfcc36700e", null ],
-      [ "HS_ERROR_NOT_FOUND", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29ba7a96460879b04fafe9d7d245d8d79364", null ],
-      [ "HS_ERROR_ACCESS", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29bac2eb59dde4e04baa25f9b6b77d39219d", null ],
-      [ "HS_ERROR_IO", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29ba0cdec99f330f67c53db313b40a4432d7", null ],
-      [ "HS_ERROR_SYSTEM", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29ba5458ec338aaf48925bb7851c415d41db", null ],
-      [ "HS_ERROR_INVALID", "group__misc.html#ggaf987ed07669903194c9f28386fc0c29ba4b1792bc59a279318b5ac668869eecdf", null ]
+    [ "hs_log_level", "group__misc.html#ga08d37b72f70a8e94bf5f545bae7bf489", [
+      [ "HS_LOG_DEBUG", "group__misc.html#gga08d37b72f70a8e94bf5f545bae7bf489ac3a100b1584db5b485ee102a8dfe781a", null ],
+      [ "HS_LOG_WARNING", "group__misc.html#gga08d37b72f70a8e94bf5f545bae7bf489ab0f2b45913ca4362a27cc36f70c6352e", null ],
+      [ "HS_LOG_ERROR", "group__misc.html#gga08d37b72f70a8e94bf5f545bae7bf489aa9b3971a8e39cbb5bbbe637a8a454b91", null ]
+    ] ],
+    [ "hs_error_code", "group__misc.html#ga81664ccf8837633feb632ed05b9ea764", [
+      [ "HS_ERROR_MEMORY", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764a0a0410182743d83784cea0cfcc36700e", null ],
+      [ "HS_ERROR_NOT_FOUND", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764a7a96460879b04fafe9d7d245d8d79364", null ],
+      [ "HS_ERROR_ACCESS", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764ac2eb59dde4e04baa25f9b6b77d39219d", null ],
+      [ "HS_ERROR_IO", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764a0cdec99f330f67c53db313b40a4432d7", null ],
+      [ "HS_ERROR_SYSTEM", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764a5458ec338aaf48925bb7851c415d41db", null ],
+      [ "HS_ERROR_INVALID", "group__misc.html#gga81664ccf8837633feb632ed05b9ea764a4b1792bc59a279318b5ac668869eecdf", null ]
     ] ],
     [ "hs_win32_release", "group__misc.html#ga707d7a2b9eb02f4303f14fc6cf1a5ff8", [
       [ "HS_WIN32_VERSION_2000", "group__misc.html#gga707d7a2b9eb02f4303f14fc6cf1a5ff8a5b58d489ac6e21a2e259d5f5f2c11eb7", null ],
@@ -28,10 +33,11 @@ var group__misc =
     ] ],
     [ "hs_version", "group__misc.html#ga9cb513f4b03c049c9a76e7e99b447937", null ],
     [ "hs_version_string", "group__misc.html#gab453230a4cc15e32755d9990c4cbc593", null ],
-    [ "hs_error_redirect", "group__misc.html#gab186b859eba142522184d17e295644e0", null ],
-    [ "hs_error_mask", "group__misc.html#ga295f20d33fcb6ead1fa415a23992fc48", null ],
+    [ "hs_log_redirect", "group__misc.html#gab765d06278769c8fe2291095267625a3", null ],
+    [ "hs_log", "group__misc.html#ga81cec63e387124d0ec01a679bd8507c2", null ],
+    [ "hs_error_mask", "group__misc.html#gaa8da9377c0c5afbb81593cb697101415", null ],
     [ "hs_error_unmask", "group__misc.html#ga295dd6900cb190f2041712747c1d3203", null ],
-    [ "hs_error", "group__misc.html#gadf53785ece9efb85de0af39a61ac12b5", null ],
+    [ "hs_error", "group__misc.html#gafdc5b9f8999146998d6bd775fd5240e4", null ],
     [ "hs_millis", "group__misc.html#gaafafebd2e1dded0a279bc22003934253", null ],
     [ "hs_adjust_timeout", "group__misc.html#gaa41d2322c14a1a759efb03ba26efc61d", null ],
     [ "hs_linux_version", "group__misc.html#ga448a59497504c07f4615db2dd8eb2243", null ],

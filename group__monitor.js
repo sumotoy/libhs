dfd80c82a78065c5c1753520cf2f6a453fe3045f
Niels Martignène <niels.martignene@gmail.com>
2015-10-31 12:06:39 +0100

    Add hs_enumerate() API

--- group__monitor.js
+++ group__monitor.js
@@ -2,11 +2,12 @@ var group__monitor =
 [
     [ "hs_monitor", "group__monitor.html#ga88e3c086804a544301f9f5e647a82c10", null ],
     [ "hs_monitor_callback_func", "group__monitor.html#gac39c55360ac625318cbab20853b768eb", null ],
+    [ "hs_enumerate", "group__monitor.html#ga2d8636aaba5b624985aea704c28ef622", null ],
     [ "hs_monitor_new", "group__monitor.html#ga799b224f0743c1bb2d039cbf03e7a176", null ],
     [ "hs_monitor_free", "group__monitor.html#gaa2c24eeaa4d0a4dbab9b9c0b07696c07", null ],
     [ "hs_monitor_get_descriptor", "group__monitor.html#ga13b6ee8a82488fbbe97ccf66ddca5f3e", null ],
     [ "hs_monitor_register_callback", "group__monitor.html#ga96e121adfa3ac5060058bec3ee9c2d89", null ],
     [ "hs_monitor_deregister_callback", "group__monitor.html#ga439f4f0a7ea840274c07b3e4be9d1b33", null ],
     [ "hs_monitor_refresh", "group__monitor.html#ga3ee6c40990a2c5d41f0b235ccc943b9f", null ],
-    [ "hs_monitor_enumerate", "group__monitor.html#ga90bf2a2b0a2d793e1ccb8975f2e9ade2", null ]
+    [ "hs_monitor_list", "group__monitor.html#ga044af9c05f22c8035009795af66d6f02", null ]
 ];
